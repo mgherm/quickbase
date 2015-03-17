db3d0963b0f5d1b1169e2268ce930f4d1fded001
mike <herman@cictr.com>
2015-03-17 07:48:23 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+__author__ = 'mike'
+
+from setuptools import setup
+
+setup(name='quickbase',
+      version='0.5',
+      description='quickbase api tools',
+      url="https://github.com/cictr/quickbase",
+      author='Mike Herman',
+      author_email='herman@cictr.com',
+      packages=['quickbase'],
+      zip_safe=False)
