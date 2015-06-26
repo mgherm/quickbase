636625c7165de003f5be8c237c9a4f1cbaf05e5b
mgherm <herman@cictr.com>
2015-06-26 13:49:48 -0400

    increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ __author__ = 'mike'
 from setuptools import setup
 
 setup(name='quickbase',
-      version='0.82',
+      version='0.83',
       description='quickbase api tools',
       url="https://github.com/cictr/quickbase",
       author='Mike Herman',
