20c4d272e79e79bc836f6255c858e9b0d23785e6
mgherm <herman@cictr.com>
2021-09-14 15:16:52 -0400

    Tried to streamline the QuickbaseAction object so it makes more sense. Added some documentation. Hopefully squashed the bug in the new by-record parser.

--- quickbase/action_test.py
+++ quickbase/action_test.py
@@ -1,4 +1,5 @@
 # import quickbase
+import datetime
 
 import __init__ as quickbase
 
@@ -17,8 +18,13 @@ def generate_quickbase_app(file='CIC.cfg'):
 if __name__ == '__main__':
     app = generate_quickbase_app()
 
-    # query = "query={3.EX.'93'}"
-    # clist = '1.2.3.4.5.6.7.8.9'
+    query = "query={3.EX.'93'}"
+    clist = '1.2.3.4.5.6.7.8.9'
     # r = quickbase.QuickbaseAction(app, 'clients', 'query', query=query, clist=clist)
-    # s = r.performAction()
-    # print(r)
+    clist = '3.778.94.306.147.301.244.34.125'
+    r = quickbase.QuickbaseAction(app, 'clients', 'edit', clist=clist, data={'8060': ['Health', 'foobsdfasdfcar foasdfo', '123 abc asdfst', 'bar asdffoo', 'Client', 'rsmith@cic.com', str(quickbase.DateToEpoch(datetime.date.today())), 'Internet Search']})
+    # r = quickbase.QuickbaseAction(app, 'clients', 'querycount', query=query, clist=clist)
+    # r = quickbase.QuickbaseAction(app, 'keys', 'purge', query=query, confirmation=True)
+
+    s = r.performAction()
+    print(r)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ __author__ = 'mike'
 from setuptools import setup
 
 setup(name='quickbase',
-      version='2.101',
+      version='2.110',
       description='quickbase api tools',
       url="https://github.com/cictr/quickbase",
       author='Mike Herman',
